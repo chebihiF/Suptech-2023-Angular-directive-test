9e1dfff6592f8824865e6fc28b883a7744b10975
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-04-15 14:18:13 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directive-test';
+  numbers = [1,2,3,4,5];
+  onlyAdd = false
 }
